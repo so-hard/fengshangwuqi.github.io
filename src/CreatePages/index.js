61e96d42ca42f835d972940881a4ec0ab0b1bd5f
枫上雾棋 <fengshangwuqi@gmail.com>
2018-02-21 01:37:18 +0800

    docs: add base docs :memo:

--- src/CreatePages/index.js
+++
@@ -1,7 +0,0 @@
-const { createTagPages } = require('./components/Tag');
-const { createPostPages } = require('./components/Post');
-const { createArchivePages } = require('./components/Archive');
-
-exports.createTagPages = createTagPages;
-exports.createPostPages = createPostPages;
-exports.createArchivePages = createArchivePages;
